ded7b206b506cd2e8b60c015d97535aa60808a12
Clement Francis Delos Santos <clementfrad27@gmail.com>
2025-04-28 00:00:31 +0800

    Updated Present Page

--- app/components/SlotMachineItem.tsx
+++ app/components/SlotMachineItem.tsx
@@ -53,30 +53,43 @@ const SlotMachineItem = ({ targetChar, delayReveal }: SlotMachineItemProps) => {
     console.log("Target: ", targetChar);
   }, [targetChar]);
   return (
-    <div className="slot-machine-item">
-      <div
-        className="slot-reel flex flex-col w-full align-center justify-center absolute top-0 left-0 transition-transform duration-1000 ease-in-out"
-        style={{
-          transform: `translateY(-${nthChar})`,
-          transitionDelay: `${delayReveal * 0.5}s`
-        }}
-      >
-        {characters.map((char, index) => (
-          <div
-            key={index}
-            className="slot-reel-item w-full aspect-square  flex justify-center items-center"
-          >
-            {char}
-          </div>
-        ))}
-        {characters.map((char, index) => (
-          <div
-            key={index}
-            className="slot-reel-item w-full aspect-square  flex justify-center items-center"
-          >
-            {char}
-          </div>
-        ))}
+    <div className="slot-reel-column flex flex-col">
+      <div className="slot-reel-upper-lights">
+        <div className="slot-reel-light-blinker"></div>
+        <div className="slot-reel-light-blinker"></div>
+        <div className="slot-reel-light-blinker"></div>
+      </div>
+      <div className="slot-reel-shell">
+        <div
+          className="slot-reel flex flex-col w-full align-center justify-center absolute top-0 left-0 transition-transform duration-1000 ease-in-out"
+          style={{
+            transform: `translateY(-${nthChar})`,
+            transitionDelay: `${delayReveal * 0.5}s`
+          }}
+        >
+          {characters.map((char, index) => (
+            <div
+              key={index}
+              className="slot-reel-item w-full aspect-square  flex justify-center items-center"
+            >
+              {char}
+            </div>
+          ))}
+          {characters.map((char, index) => (
+            <div
+              key={index}
+              className="slot-reel-item w-full aspect-square  flex justify-center items-center"
+            >
+              {char}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="slot-reel-lower-lights">
+        <div className="slot-reel-light-blinker"></div>
+        <div className="slot-reel-light-blinker"></div>
+        <div className="slot-reel-light-blinker"></div>
       </div>
     </div>
   );
--- app/routes/present.tsx
+++ app/routes/present.tsx
@@ -4,28 +4,30 @@ import SlotMachineItem from "~/components/SlotMachineItem";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Kopiko Blanca Raffle" },
     { name: "description", content: "Welcome to React Router!" }
   ];
 }
 
 export default function Present() {
+  const ANCode = "JOHN0CNRDO";
+
   return (
     <main className="flex items-center justify-center pt-16 pb-4 flex-col gap-4">
       <h1>Sample Title</h1>
       <div className="slot-machine-shell">
         <div className="slot-machine select-none">
           <div className="slot-machine-inner flex">
-            <SlotMachineItem targetChar={"K"} delayReveal={1} />
-            <SlotMachineItem targetChar={"O"} delayReveal={2} />
-            <SlotMachineItem targetChar={"P"} delayReveal={3} />
-            <SlotMachineItem targetChar={"I"} delayReveal={4} />
-            <SlotMachineItem targetChar={"K"} delayReveal={5} />
-            <SlotMachineItem targetChar={"O"} delayReveal={6} />
-            <SlotMachineItem targetChar={"B"} delayReveal={7} />
-            <SlotMachineItem targetChar={"L"} delayReveal={8} />
-            <SlotMachineItem targetChar={"N"} delayReveal={9} />
-            <SlotMachineItem targetChar={"C"} delayReveal={10} />
+            <SlotMachineItem targetChar={ANCode[0]} delayReveal={1} />
+            <SlotMachineItem targetChar={ANCode[1]} delayReveal={2} />
+            <SlotMachineItem targetChar={ANCode[2]} delayReveal={3} />
+            <SlotMachineItem targetChar={ANCode[3]} delayReveal={4} />
+            <SlotMachineItem targetChar={ANCode[4]} delayReveal={5} />
+            <SlotMachineItem targetChar={ANCode[5]} delayReveal={6} />
+            <SlotMachineItem targetChar={ANCode[6]} delayReveal={7} />
+            <SlotMachineItem targetChar={ANCode[7]} delayReveal={8} />
+            <SlotMachineItem targetChar={ANCode[8]} delayReveal={9} />
+            <SlotMachineItem targetChar={ANCode[9]} delayReveal={10} />
           </div>
         </div>
       </div>
