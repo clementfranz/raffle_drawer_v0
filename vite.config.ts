5bd37ff8267e56ca71ec1ffbcf2d1e6761bf2e6c
Clement Francis Delos Santos <clementfrad27@gmail.com>
2025-05-04 08:39:51 +0800

    updated to be built as SPA and PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,33 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()]
+  plugins: [
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Your App Name",
+        short_name: "App",
+        description: "A description of your app",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/KopikoBlancaLogoTrimmed.ico",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icons/KopikoBlancaLogoTrimmed.ico",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    }),
+    reactRouter(),
+    tsconfigPaths()
+  ]
 });
