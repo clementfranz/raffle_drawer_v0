8e8844b911ad7a01853bcef46ebf331faf0524c8
Clement Francis Delos Santos <clementfrad27@gmail.com>
2025-05-16 18:06:52 +0800

    Fixed modals for clearing participants confirmation

--- app/components/ControlPanelTabs/Tab_Overview/components/ClearAllPaticipantsButton/ClearAllPaticipantsButton.tsx
+++ app/components/ControlPanelTabs/Tab_Overview/components/ClearAllPaticipantsButton/ClearAllPaticipantsButton.tsx
@@ -3,35 +3,28 @@ import useLocalStorageState from "use-local-storage-state";
 import { clearAllParticipants } from "~/hooks/indexedDB/participant/clearAllParticipants";
 import TabActionButton from "~/ui/ControlPanelUI/TabActionButton/_main/TabActionButton";
 
-const ClearAllPaticipantsButton = () => {
-  const [fileDetails, setFileDetails] = useLocalStorageState<Object | null>(
-    "fileDetails",
-    { defaultValue: null }
-  );
+type ClearAllParticipantsButtonPropsTypes = {
+  openModal: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
+const ClearAllPaticipantsButton = ({
+  openModal
+}: ClearAllParticipantsButtonPropsTypes) => {
   const [withParticipantsData, setWithParticipantsData] = useLocalStorageState(
     "withParticipantsData",
     { defaultValue: false }
   );
-  const [regionalStats, setRegionalStats] =
-    useLocalStorageState("regionalStats");
+
+  const handleOpenModal = () => {
+    openModal(true);
+  };
 
   const [clearParticipantsLoading, setClearParticipantsLoading] =
     useLocalStorageState("clearParticipantsLoading", { defaultValue: false });
 
-  const handleClearAllParticipants = async () => {
-    const clearSuccess = await clearAllParticipants();
-    if (clearSuccess) {
-      setWithParticipantsData(false);
-      console.log("All Participants Cleared");
-      setRegionalStats([]);
-      setFileDetails(null);
-      // window.location.reload();
-    }
-  };
   return (
     <>
-      <TabActionButton onClick={handleClearAllParticipants}>
+      <TabActionButton onClick={handleOpenModal}>
         Clear All Participants
       </TabActionButton>
     </>
